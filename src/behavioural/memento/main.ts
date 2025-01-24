13b0370b538520617309b81bde80d6a00b9c57cf
Rodrigo Sumida <sumidarodrigo@gmail.com>
2025-01-24 09:43:57 -0300

    Memento

---
+++ src/behavioural/memento/main.ts
@@ -0,0 +1,29 @@
+import { ImageEditor } from './image-editor';
+import { ImageEditorBackupManager } from './image-editor-backup-manager';
+
+const imageEditor = new ImageEditor('/media/image.png', 'png');
+const backupManager = new ImageEditorBackupManager(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo('gif');
+console.log(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo('jpg');
+console.log(imageEditor);
+
+backupManager.backup();
+imageEditor.convertFormatTo('bmp');
+console.log(imageEditor);
+
+backupManager.undo();
+console.log(imageEditor);
+
+backupManager.undo();
+console.log(imageEditor);
+
+backupManager.undo();
+console.log(imageEditor);
+
+backupManager.undo();
+console.log(imageEditor);
